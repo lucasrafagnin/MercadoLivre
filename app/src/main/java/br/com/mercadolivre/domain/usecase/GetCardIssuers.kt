6f9a43efb93d40fb7b869c9b09054f46085a2b38
Lucas Rafagnin <lucasrafagnin7@gmail.com>
2018-12-02 18:05:56 -0200

    Minor adjusts

--- app/src/main/java/br/com/mercadolivre/domain/usecase/GetCardIssuers.kt
+++ app/src/main/java/br/com/mercadolivre/domain/usecase/GetCardIssuers.kt
@@ -3,7 +3,7 @@ package br.com.mercadolivre.domain.usecase
 import br.com.mercadolivre.data.model.CardIssuer
 import br.com.mercadolivre.data.repository.PaymentRepository
 import io.reactivex.Observable
-import mobi.porquenao.sovai.exception.EmptyStateException
+import br.com.mercadolivre.exception.EmptyStateException
 import javax.inject.Inject
 
 class GetCardIssuers @Inject constructor(
