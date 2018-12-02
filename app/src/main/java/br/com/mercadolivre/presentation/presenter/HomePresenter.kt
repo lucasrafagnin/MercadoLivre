3a56a32826ef8294b83b26c1ed87a650fbcf2983
Lucas Rafagnin <lucasrafagnin7@gmail.com>
2018-12-02 18:49:04 -0200

    Open payment flow

--- app/src/main/java/br/com/mercadolivre/presentation/presenter/HomePresenter.kt
+++ app/src/main/java/br/com/mercadolivre/presentation/presenter/HomePresenter.kt
@@ -1,7 +1,18 @@
 package br.com.mercadolivre.presentation.presenter
 
 import br.com.mercadolivre.base.BasePresenter
+import br.com.mercadolivre.domain.usecase.SavePaymentCache
+import br.com.mercadolivre.navigation.NavigationController
 import br.com.mercadolivre.ui.activity.HomeView
 import javax.inject.Inject
 
-class HomePresenter @Inject constructor() : BasePresenter<HomeView>()
+class HomePresenter @Inject constructor(
+        private val navigation: NavigationController,
+        private val savePaymentCache: SavePaymentCache
+) : BasePresenter<HomeView>() {
+
+    fun launchPaymentMethod() = navigation.launchPayment()
+
+    fun savePrice(price: Double) = savePaymentCache.execute(price)
+
+}
