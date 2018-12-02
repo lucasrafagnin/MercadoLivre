3fa958dcb9cfd49478462f37d933c082d5de23d8
Lucas Rafagnin <lucasrafagnin7@gmail.com>
2018-12-02 00:46:23 -0200

    Home structure

--- app/src/main/java/br/com/mercadolivre/ui/activity/HomeActivity.kt
+++ app/src/main/java/br/com/mercadolivre/ui/activity/HomeActivity.kt
@@ -3,14 +3,31 @@ package br.com.mercadolivre.ui.activity
 import android.os.Bundle
 import br.com.mercadolivre.R
 import br.com.mercadolivre.base.BaseActivity
+import br.com.mercadolivre.base.BaseView
+import br.com.mercadolivre.presentation.presenter.HomePresenter
+import kotlinx.android.synthetic.main.home.*
+import javax.inject.Inject
 
-class HomeActivity : BaseActivity() {
+interface HomeView : BaseView
+
+class HomeActivity : BaseActivity(), HomeView {
+
+    @Inject
+    lateinit var presenter: HomePresenter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.home)
-
         viewComponent.inject(this)
+
+        action.setOnClickListener {}
+
+        presenter.onAttachView(this)
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        presenter.onDetachView()
     }
 
 }
